3bf4326fdad1229a2995c48bdfe68bf69e8aeaee
aden <s251545@students.aie.edu.au>
2026-08-25 13:37:31 +0930

    Adjustments
    thats all

--- Echoes-of-the-Deep/Assets/Scripts/Main Menu/Main Menu.cs
+++ Echoes-of-the-Deep/Assets/Scripts/Main Menu/Main Menu.cs
@@ -5,19 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-    void Update()
-    {
-        
-    }
-
     public void Play() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    /*public void Options() 
+    {
+        SceneManager.LoadScene("Settings");
+    }*/
 
     public void Quit()
     {
