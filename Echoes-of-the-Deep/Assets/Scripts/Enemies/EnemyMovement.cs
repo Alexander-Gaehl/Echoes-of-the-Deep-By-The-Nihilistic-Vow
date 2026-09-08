a86b121659c4f83ad2fdc24d2244e2177c0a1947
Jirou-Kyouka <alexander@gaehl.com>
2026-09-08 14:20:33 +0930

    made enemy

---
+++ Echoes-of-the-Deep/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 2f;
+
+    Rigidbody2D rb;
+
+    public Transform target;
+
+    Vector2 moveDirection;
+
+    private bool follow;
+
+    void Awake()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 direction = (target.position - transform.position).normalized;
+
+        moveDirection = direction;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        follow = true;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        follow = false;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (follow)
+        {
+
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+
+        }
+        else
+        {
+
+            rb.velocity = new Vector2(0, 0);
+
+        }
+
+    }
+
+}
